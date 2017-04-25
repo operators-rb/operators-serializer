769865716d23a81ab19bfd666202d1fe598de484
Kirill Shevchenko <kirills167@gmail.com>
2017-04-25 13:01:58 +0300

    init operators-serializer

---
+++ lib/operators/serializer.rb
@@ -0,0 +1,7 @@
+require "operators/serializer/version"
+
+module Operators
+  module Serializer
+    # Your code goes here...
+  end
+end
---
+++ lib/operators/serializer/version.rb
@@ -0,0 +1,5 @@
+module Operators
+  module Serializer
+    VERSION = "0.1.0"
+  end
+end
